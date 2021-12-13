94d472bda5439111a629ea10fa15445879b796b8
dmgould2 <96065229+dmgould2@users.noreply.github.com>
2021-12-13 08:48:31 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,21 @@
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . f f . . . . . . 
-    . . . . . . . f f f . . . . . . 
-    . . . . . . . f e f f . . . . . 
-    . . . . . . f f f e f . . . . . 
-    . . . . . . f f f e f . . . . . 
-    . . . . . . f f e f f . . . . . 
-    . . . . . f f f e f . . . . . . 
-    . . . . . f e f e f . . . . . . 
-    . . . f f f e f f f f f . . . . 
-    . . . f e e f f e e f f . . . . 
-    . . . f e e f e e e e f . . . . 
-    . . . f f e f f e e e f . . . . 
-    . . . . f f f f f e e f . . . . 
-    . . . . . . . . f f f f . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+let mySprite: Sprite = null
+forever(function () {
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . f f . . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . . f e f f . . . . . 
+        . . . . . . f f f e f . . . . . 
+        . . . . . . f f f e f . . . . . 
+        . . . . . . f f e f f . . . . . 
+        . . . . . f f f e f . . . . . . 
+        . . . . . f e f e f . . . . . . 
+        . . . f f f e f f f f f . . . . 
+        . . . f e e f f e e f f . . . . 
+        . . . f e e f e e e e f . . . . 
+        . . . f f e f f e e e f . . . . 
+        . . . . f f f f f e e f . . . . 
+        . . . . . . . . f f f f . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+})
